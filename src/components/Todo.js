854c73988acb53fdcb672a363fc7eedf6ce68e79
Zakiah07 <zakiah_bloom@yahoo.com>
2022-07-17 10:11:53 +0800

    fix typo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 // import TodoList from './TodoList'
-import { RiCloseCircleLine } from 'react-icon/ri'
-import { TiEdit } from 'react-icon/ti'
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { TiEdit } from 'react-icons/ti'
 
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
